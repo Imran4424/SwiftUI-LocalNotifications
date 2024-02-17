eb5c52e51798d8cb9ff7efdfbc1e41325fdd4c75
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-02-17 21:18:50 +0600

    project init

---
+++ LocalNotifications/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalNotifications
+//
+//  Created by Shah Md Imran Hossain on 17/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalNotifications/LocalNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationsApp.swift
+//  LocalNotifications
+//
+//  Created by Shah Md Imran Hossain on 17/2/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
